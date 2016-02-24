28ee932c0c247ca5a3275e00823d8d6e829df0ee
Nguyen Thi Phuong Thuy <p-thuy@oa.netmile.lan>
2016-02-24 18:54:17 +0900

    test nhieu dong

--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -1,41 +1,43 @@
 $(document).ready(function(){
 
+  function applyAll(table, col, inputType, fromRow, toRow){
+    var value = "";
+    index = col.index();
+    switch (inputType){
+      case 1:
+        value = col.find("input").val();
+      break;
+      case 2:
+        value = col.find("select option:selected").text();
+      break;
+      case 3:
+      break;
+      default:
+      alert("Sorry, can't apply to all!");
+    }
+    if(value == ""){
+      return
+    }
+    
+    table.find("tr").each(function(){
+      if ($(this).index() <= fromRow) {
+        return;
+      };
+      if ($(this).index() > toRow && toRow >= 0) {
+        return;
+      };
+      $(this).find("td:eq("+index+")").text(value);
+    });
+  }
+
   if($("#targetTable").length){
-
-    var typeInput = 1, typeDrop = 2;
-
-    function applyAllInputText(){
-      value = td.find("input").val();
-      if(value == ""){
-        return
-      }
-      $("#targetTable tr").each(function(){
-        if ($(this).index() <= skipIndex) {
-          return 
-        }
-        $(this).find("td:eq("+index+")").text(value);
-      });
-    }
-
-    function applyAllDropdown(){
-      console.log("huehue");
-    }
-
     $("#targetTable").on("click", ".applyAll", function(){
-
+      table = $(this).closest("table");
       skipIndex = $(this).closest("tr").index();
       td = $(this).closest("td");
-      index = td.index();
-      switch ($(this).data("type")){
-        case typeInput:
-          applyAllInputText();
-          break;
-        case typeInput:
-          applyAllDropdown();
-          break;
-        default:
-          console.log("Sorry, we are out of .");
-      }
+      inputType = $(this).data("type");
+
+      applyAll(table, td, inputType, skipIndex, -1);
     });
   }
 
